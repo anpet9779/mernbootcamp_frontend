1c78a0aae6928d9b0114abacd80b1d6ca6e34551
parag petkar <parag.petkar@smartdatainc.net>
2021-11-02 21:08:50 +0530

    user authentication

---
+++ src/user/Signin.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+
+const Signin = () => {
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group py-3">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="text" />
+            </div>
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <button className="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Signin" description="A Page for user login">
+      <h1>Sign in</h1>
+      {signInForm()}
+    </Base>
+  );
+};
+
+export default Signin;
